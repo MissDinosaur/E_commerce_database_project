9da9912c30759a93aeeb51850a731df118479187
unknown <1157363323@qq.com>
2025-02-14 17:35:23 +0100

    modify data ingestion and data query part

--- backend/app/routes/products.py
+++ backend/app/routes/products.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends
 from app.models.database import get_db
+from sqlalchemy.sql import text
 
 router = APIRouter()
 
 @router.get("/products")
 def get_products():
     db = next(get_db())
-    result = db.execute("SELECT product_id, product_category_name, price FROM products LIMIT 2").fetchall()
+    result = db.execute(text("SELECT product_id, product_category_name, price FROM products LIMIT 2")).fetchall()
     products = [{"product_id": row[0], "product_category_name": row[1], "price": row[2]} for row in result]
     return products
