3a3bc4b46c38dccc390c3621c3d88612118d4d2e
unknown <1157363323@qq.com>
2025-02-18 17:59:45 +0100

    Streamlit

--- streamlit/pages/1_PostgreSQL_show.py
+++ streamlit/pages/1_PostgreSQL_show.py
@@ -2,16 +2,18 @@ import pandas as pd
 from sqlalchemy import create_engine, Table, MetaData, select, func
 from sqlalchemy.engine import reflection
 import streamlit as st
+import os
 
-POSTGRES_URL = "postgresql://postgres:password@localhost:5432/ecommerce"
+POSTGRES_URL = f"postgresql://{os.getenv('POSTGRES_USER', 'postgres')}:{os.getenv('POSTGRES_PASSWORD', 'password')}@{os.getenv('POSTGRES_HOST', 'localhost')}:{os.getenv('POSTGRES_PORT', '5432')}/{os.getenv('POSTGRES_DB', 'ecommerce')}"
+
 
 print(POSTGRES_URL)
 # Connect to PostgreSQL
 pg_engine = create_engine(POSTGRES_URL)
 inspector = reflection.Inspector.from_engine(pg_engine)
 metadata = MetaData()
-
-tables_names = ['order_items', 'order_payments', 'order_reviews', 'orders', 'customers', 'geolocation', 'products', 'product_category_name_translation', 'sellers', 'leads_qualified', 'leads_closed']
+table_names = inspector.get_table_names()
+#table_names = ['order_items', 'order_payments', 'order_reviews', 'orders', 'customers', 'geolocation', 'products', 'product_category_name_translation', 'sellers', 'leads_qualified', 'leads_closed']
 
 def get_all_columns(table_name):
     columns = inspector.get_columns(table_name)
@@ -20,13 +22,14 @@ def get_all_columns(table_name):
         column_names[col.get("name")] = col.get("type")
     return column_names
     
-def show_postgres_tables(tables_names: str):
-    st.write("We have below tables in our Postgresql:")
-    st.write(f"{tables_names}")
+def show_postgres_tables(table_names: str):
+    st.write(f"We have below {len(table_names)} tables in our Postgresql with DB = ecommerce.")
+    st.write("They are namely")
+    st.write(f"{', '.join(table_names)}.")
     
     tables = []
     table_schemas = []
-    for name in tables_names:
+    for name in table_names:
         table = Table(name, metadata, autoload_with=pg_engine)
         tables.append(table)
         columns = inspector.get_columns(name)
@@ -38,6 +41,7 @@ def show_postgres_tables(tables_names: str):
             dict["col_type"] = str(column.get("type"))
             table_schemas.append(dict)
 
+    st.write("And here are their schemas.")
     df = pd.DataFrame(table_schemas)
     st.dataframe(df)
 
@@ -66,13 +70,13 @@ def check_data(table_option, columns_option, size: int, filter):
 if __name__ == "__main__":   
     st.write("# Welcme to PostgreSQL")
     st.write("* **Table summary**")
-    show_postgres_tables(tables_names)
+    show_postgres_tables(table_names)
 
 
     st.write("* **Check data**")
     col1, col2 = st.columns(2)
     with col1:
-        table_option = st.selectbox("Select table", tuple(tables_names))
+        table_option = st.selectbox("Select table", tuple(table_names))
         columns_option = st.multiselect("Select columns", ("All", ) + tuple(get_all_columns(table_option).keys()))
     with col2:
         st.write("Filter condition")
